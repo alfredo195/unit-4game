015be87ef6fcf27cf0266f29b99724604b713d0c
BlackAce <alfredo.gon195@gmail.com>
2019-09-04 00:48:53 -0700

    creat function to reset game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,37 @@
-$(document).ready(function(){
-    //generate random number to guess
-    var random = Math.floor(Math.random()*102+19)
+$(document).ready(function() {
+  //generate random number to guess
+  var random = Math.floor(Math.random() * 102 + 19);
 
-    // display of radom number
+  // display of radom number
 
+  $("#scoreToMatch").text(random);
+
+  //random number for crystals
+  var num1 = Math.floor(Math.random() * 12 + 1);
+  var num2 = Math.floor(Math.random() * 12 + 1);
+  var num3 = Math.floor(Math.random() * 12 + 1);
+  var num4 = Math.floor(Math.random() * 12 + 1);
+
+  //var to track win, losses, total
+
+  var wins = 0;
+  var losses = 0;
+  var playerTotal = 0;
+
+  $("#wins").text(wins);
+  $("#losses").text(losses);
+
+  // restart game
+
+  function reset() {
+    random = Math.floor(Math.random() * 102 + 19);
+    console.log(random);
     $("#scoreToMatch").text(random);
-
-    //random number for crystals
-    var num1 = Math.floor(Math.random()*12+1)
-    var num2 = Math.floor(Math.random()*12+1)
-    var num3 = Math.floor(Math.random()*12+1)
-    var num4 = Math.floor(Math.random()*12+1) 
-
-    //var to track win, losses, total
-
-    var wins = 0;
-    var losses = 0;
-    var playerTotal =0;
-})
-
+    num1 = Math.floor(Math.random() * 12 + 1);
+    num2 = Math.floor(Math.random() * 12 + 1);
+    num3 = Math.floor(Math.random() * 12 + 1);
+    num4 = Math.floor(Math.random() * 12 + 1);
+    playerTotal=0;
+    $("score").text(playerTotal);
+  }
+});
